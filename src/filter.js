8de5df5f5cb333c881c0e5c2a4a19ad79d751a79
Dominique Hazael-Massieux <dom@w3.org>
2019-03-13 14:22:13 +0100

    Add spec filter to translation matrix

---
+++ src/filter.js
@@ -0,0 +1,29 @@
+const debounce = (fn, time) => {
+  let timeout;
+
+  return function() {
+    const functionCall = () => fn.apply(this, arguments);
+    clearTimeout(timeout);
+    timeout = setTimeout(functionCall, time);
+  }
+}
+
+const specs = document.getElementById("specs");
+const filterInput = document.createElement("input");
+filterInput.setAttribute("aria-label", "Filter list of specifications by title or URL");
+const rows = [...document.querySelector("table").querySelectorAll("tr")];
+
+filterInput.addEventListener("input", debounce(function(ev) {
+  const match = new RegExp(ev.target.value, "i");
+  rows.forEach(tr => {
+    const th = tr.querySelector("th");
+    if (!th || !th.querySelector("a")) return;
+    if (!th.textContent.match(match) && !th.querySelector("a").href.match(match)) {
+      tr.classList.add("hidden");
+    } else {
+      tr.classList.remove("hidden");
+    }
+  });
+}, 300));
+specs.appendChild(document.createElement("br"));
+specs.appendChild(filterInput);
