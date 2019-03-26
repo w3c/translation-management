4a3019ada7269d144f398dd25de63ea7f76e6cff
Dominique Hazael-Massieux <dom@w3.org>
2019-03-26 12:59:52 +0100

    Generate translation pages with Jekyll

--- src/_site/filter.js
+++ src/_site/filter.js
@@ -12,6 +12,7 @@ const specs = document.getElementById("specs");
 const filterInput = document.createElement("input");
 filterInput.setAttribute("aria-label", "Filter list of specifications by title or URL");
 const rows = [...document.querySelector("table").querySelectorAll("tr")];
+const tbodies = [...document.querySelector("table").querySelectorAll("tbody")];
 
 filterInput.addEventListener("input", debounce(function(ev) {
   const match = new RegExp(ev.target.value, "i");
@@ -22,6 +23,13 @@ filterInput.addEventListener("input", debounce(function(ev) {
       tr.classList.add("hidden");
     } else {
       tr.classList.remove("hidden");
+    }
+  });
+  tbodies.forEach(tb => {
+    if (tb.querySelectorAll("tr:not(.hidden)").length <= 1) {
+      tb.querySelector("tr").classList.add("hidden");
+    } else {
+      tb.querySelector("tr").classList.remove("hidden");
     }
   });
 }, 300));
