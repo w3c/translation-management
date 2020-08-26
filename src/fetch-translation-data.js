22116c09a7bf240bf0a577c9df2f4124219394d0
Dominique Hazael-Massieux <dom@w3.org>
2020-08-26 16:17:47 +0200

    Fix bug in collecting translation data for the matrix when multiple versions of a doc exist
    The script used to stop at the first (in general, oldest) rec when several call for translations exist

--- src/fetch-translation-data.js
+++ src/fetch-translation-data.js
@@ -35,8 +35,8 @@ w3c.recommendations().fetch({embed: true}, function(err, recs) {
             r.date = r._links["latest-version"].href.split('/').pop().replace(/([0-9]{4})([0-9]{2})([0-9]{2})/, '$1-$2-$3');
             r.year = r._links["latest-version"].href.split('/').pop().slice(0,4);
             r.translations = {};
-            const cft = groupedTranslations.find(s => s["spec-version"]._links.specification.href === r._links.self.href);
-            if (cft) {
+            const cfts = groupedTranslations.filter(s => s["spec-version"]._links.specification.href === r._links.self.href);
+            cfts.forEach(cft => {
               r.translations = Object.keys(langs).reduce((obj, lang) => {
                 obj[lang] = cft.translations.find(t => t.language === lang);
                 if (obj[lang]) {
@@ -51,7 +51,7 @@ w3c.recommendations().fetch({embed: true}, function(err, recs) {
                 }
                 return obj;
               }, {});
-            }
+            })
             return r;
           });
 
